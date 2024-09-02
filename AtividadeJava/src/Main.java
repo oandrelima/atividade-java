1326d7ae5156f53dc90c7e362070bbe5f38fa2e1
oandrelima <oandreluislima@gmail.com>
2024-09-02 00:46:41 -0300

    Main.java

--- AtividadeJava/src/Main.java
+++ AtividadeJava/src/Main.java
@@ -1,19 +1,3 @@
-/*
-Entrega da Atividade 1 - Algoritmos e Programação II
-
-Nós,
-
-André Luis Lima de Oliveira
-Renan Lallo de Moura Cavalcante
-
-declaramos que
-
-todas as respostas são fruto de nosso próprio trabalho,
-não copiamos respostas de colegas externos à equipe,
-não disponibilizamos nossas respostas para colegas externos ao grupo e
-não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
-*/
-
 import java.util.Random;
 import java.util.Scanner;
 
